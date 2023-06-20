1a5a48321cb3de4ac01c4a6c0a09289d49b29707
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2023-06-20 22:29:27 +0530

    if else

---
+++ JUNE/if-else.cpp
@@ -0,0 +1,36 @@
+/*
+10:16 pm
+Tuesday, 20 June 2023 (IST)
+*/
+
+/*
+if else
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout << "Enter a number : \n";
+
+    cin >> n;
+
+    if(n % 2 == 0){
+        
+        cout << "Even";
+        
+    } else {
+
+        cout << "Odd";
+    }
+
+
+
+
+
+
+    return 0;
+}

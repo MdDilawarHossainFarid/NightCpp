13603785405f99ca184beb30f6c44e8ead61e071
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2023-06-27 23:40:03 +0530

    logical operators

---
+++ JUNE/operators/logical/logical.cpp
@@ -0,0 +1,31 @@
+/*
+11:00 pm
+Tuesday, 27 June 2023 (IST)*/
+
+// Logical operators
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // int a = 1, b = 1;
+
+    // int a = 0, b = 0; 
+
+    int a = 1, b = 0;
+
+  
+    cout << "a && b is " << (a && b) << '\n';
+
+    cout << "a || b is " << (a || b) << '\n';
+
+    cout << " !a  is " << (!a) << '\n';
+
+
+
+
+
+
+    return 0;
+}
---
+++ JUNE/while.cpp
@@ -0,0 +1,35 @@
+/*
+7:18 pm
+Monday, 26 June 2023 (IST)
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int i = 1, n;
+
+    // cin >> n;
+
+    // while(i <= n){
+
+    //     cout<<" Before increment i become " << i << '\n';
+
+    //     i++;
+
+    //     cout<<" After increment i become " << i << '\n';
+    // }
+
+    // cout << "  " << i;
+
+    {
+        int i = 0;
+
+        i++;
+
+        cout << i;
+    }
+
+    return 0;
+}
